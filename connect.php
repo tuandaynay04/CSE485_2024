946e91f3ca84b4780d7dad871c2ff819564b6720
tuandaynay04 <148810570+tuandaynay04@users.noreply.github.com>
2024-10-02 02:50:11 +0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,14 +1,21 @@
 <?php
-$servername = "localhost"; // Tên server của bạn
-$username = "root";        // Username MySQL
-$password = "";            // Mật khẩu MySQL (nếu có)
-$dbname = "btth01_cse485"; // Tên cơ sở dữ liệu
+$host = '127.0.0.1';
+$db   = 'btth01';
+$user = 'root'; 
+$pass = ''; 
+$charset = 'utf8mb4';
 
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-// Kiểm tra kết nối
-if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Kết nối thành công!";
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
